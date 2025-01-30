c9826f3efb65d90985e4c73ae0933a0a0f22099f
CabajarLeonardo <168710786+LeiLei16@users.noreply.github.com>
2025-01-30 13:17:17 +0800

    Add files via upload
    MultiTextInput

---
+++ Activity 2/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {TextInput, StyleSheet} from 'react-native';
+import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+const MultilineTextInputExample = () => {
+  const [value, onChangeText] = React.useState('About Me: ');
+
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={[
+          styles.container,
+          {
+            backgroundColor: value,
+          },
+        ]}>
+        <TextInput
+          editable
+          multiline
+          numberOfLines={4}
+          maxLength={40}
+          onChangeText={text => onChangeText(text)}
+          value={value}
+          style={styles.textInput}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    borderBottomColor: '#000',
+    borderBottomWidth: 1,
+  },
+  textInput: {
+    padding: 10,
+  },
+});
+
+export default MultilineTextInputExample;
